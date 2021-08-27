0bb60adb26808cfe5ba195d80a87aa146d52fd7a
mariana <a01412784@tec.mx>
2021-08-27 12:21:37 -0500

    complete el ejercicio 1

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,7 +1,22 @@
 def main():
-    # escribe tu código abajo de esta línea
+    # escribe tu código abajo de esta línea m = (y2 - y1) / (x2 - x1)
+    """" Dame
+    x1: 3.6
+    Dame
+    y1: -1.3
+    Dame
+    x2: 8.6
+    Dame
+    y2: 2.5
+    Pendiente: 0.76 """
+    x1 = float(input("Dame x1: "))
+    y1 = float(input("Dame y1: "))
+    x2 = float(input("Dame x2: "))
+    y2 = float(input("Dame y2: "))
+
+    m = (y2 - y1) / (x2 - x1)
+    print("Pendiente:", m)
+
     pass
-
-
 if __name__ == '__main__':
     main()
