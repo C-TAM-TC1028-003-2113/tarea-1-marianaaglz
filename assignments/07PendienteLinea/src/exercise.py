b9661a6960aec5fba6026f65800065f41508dfe7
mariana <a01412784@tec.mx>
2021-08-27 12:41:43 -0500

    complete el ejercicio 2

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,14 +1,11 @@
 def main():
     # escribe tu código abajo de esta línea m = (y2 - y1) / (x2 - x1)
-    """" Dame
-    x1: 3.6
-    Dame
+    """"x1: 3.6
     y1: -1.3
-    Dame
     x2: 8.6
-    Dame
     y2: 2.5
-    Pendiente: 0.76 """
+    m: 0.76 """
+
     x1 = float(input("Dame x1: "))
     y1 = float(input("Dame y1: "))
     x2 = float(input("Dame x2: "))
@@ -17,6 +14,6 @@ def main():
     m = (y2 - y1) / (x2 - x1)
     print("Pendiente:", m)
 
-    pass
+
 if __name__ == '__main__':
     main()
--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,7 +1,18 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    """
+    Dame el número de mensajes: 38
+    Dame el número de megas: 3.1
+    Dame el número de minutos: 78
+    El costo mensual es: 95.28
+    """
 
+    num_mensajes = int(input("Dame el número de mensajes: "))
+    num_megas = float(input("Dame el número de megas: "))
+    num_minutos = int(input("Dame el número de minutos: "))
+
+    costo_mensual = ((num_mensajes*0.8) + (num_megas*0.8) + (num_minutos*0.8))
+    print("El costo mensual es:", costo_mensual)
 
 if __name__ == '__main__':
     main()
