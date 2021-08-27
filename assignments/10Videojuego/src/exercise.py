ec95b1fa0df9c275a56cd25cc4aaae4cd70a6a44
mariana <a01412784@tec.mx>
2021-08-27 12:46:56 -0500

    complete el ejercicio 3

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,7 +1,17 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
 
+"""
+Dame la cantidad de juegos nuevos: 2
+Dame la cantidad de juegos usados: 3
+El total de la compra es: 3050
+"""
+
+    juegos_nuevos = int(input("Dame la cantidad de juegos nuevos: "))
+    juegos_usados = int(input("Dame la cantidad de juegos usados: "))
+
+    total = ((juegos_nuevos*1000) + (juegos_usados*350))
+    print("El total de la compra es:", total)
 
 if __name__ == '__main__':
     main()
